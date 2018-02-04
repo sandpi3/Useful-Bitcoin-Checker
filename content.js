2b7a7b457e1a06a4e319f94ee8f79d1a63717df4
Alex <Alex@Macbook-Pro-4.local>
2018-02-04 00:41:43 -0500

    changed stuff v2

--- content.js
+++ content.js
@@ -1,50 +1,54 @@
-function check(){
-    var bitcoinOpen = "https://api.coindesk.com/v1/bpi/historical/open.json";
-    var bitcoinCurrent = "https://api.coindesk.com/v1/bpi/currentprice.json";
-
-    var s;
-
-    var open;
-    var current = "0.0";
-
-    $.ajax({url: bitcoinOpen, dataType: 'json', async: false, success:function(data) {
-        var i = 0;
-        for (var key in data.bpi) {
-            if (i == Object.keys(data.bpi).length - 1 && data.bpi.hasOwnProperty(key)) {           
-                open = data.bpi[key];
-                open = open.replace(",", "");
-            }
-            ++i;
-        }
-    }});
-    
-    $.ajax({url: bitcoinCurrent, dataType: 'json', async: false, success:function(data) {
-        current = data.bpi.USD.rate;
-        current = current.replace(",", "");
-    }});
-
-    var answer = "Unknown";
-
-    var pElement = document.getElementById('answer');
-    if (current > open){
-        answer = "Yes";
-        pElement.setAttribute("style", "font-weight: bold; color: #00ff00;")
-    }else{
-        answer = "No"
-        pElement.setAttribute("style", "font-weight: bold; color: #ff0000;")
-    }
-
-    pElement.textContent = answer;
-
-    var cElement = document.getElementById('cprice');
-    cElement.textContent = "Current exchange to USD: $" + current;
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('recheck');
     checkPageButton.addEventListener('click', function() {
 
-        check();
-     
+        var bitcoinOpen = "https://api.coindesk.com/v1/bpi/historical/open.json";
+        var bitcoinCurrent = "https://api.coindesk.com/v1/bpi/currentprice.json";    
+        var open;
+        var current;
+    
+        $.ajax({url: bitcoinOpen, dataType: 'json', async: false, success:function(data) {
+            var i = 0;
+            for (var key in data.bpi) {
+                if (i == Object.keys(data.bpi).length - 1 && data.bpi.hasOwnProperty(key)) {           
+                    open = data.bpi[key];
+                    open = open.replace(",", "");
+                }
+                ++i;
+            }
+        }});
+        
+        $.ajax({url: bitcoinCurrent, dataType: 'json', async: false, success:function(data) {
+            current = data.bpi.USD.rate;
+            current = current.replace(",", "");
+        }});
+    
+        var answer = "Unknown";
+        var reaction = "(Probably not though)";
+
+        var noAnswers = ["That's not surprising at all", "Only in your imagination", "Why would you think that?", "Never believe it would happen", "How did it even go up in the first place?", "Maybe in a parallel universe"];
+        var yesAnswers = ["How?", "What in the world?", "Well that's a Surprise", "I'm genuinely amazed", "Well I'll be damned", "Looks like miracles do happen"];
+    
+        var pElement = document.getElementById('answer');
+        var rElement = document.getElementById('reaction');
+        if (current > open){
+            answer = "Yes";
+            reaction = yesAnswers[Math.floor(Math.random() * yesAnswers.length)];
+            rElement.setAttribute("style", "font-weight: bold; color: #62ff62;")
+            pElement.setAttribute("style", "font-weight: bold; color: #62ff62;")
+        }else{
+            answer = "No"
+            reaction = noAnswers[Math.floor(Math.random() * noAnswers.length)];
+            rElement.setAttribute("style", "font-weight: bold; color: #ff0000;")
+            pElement.setAttribute("style", "font-weight: bold; color: #ff0000;")
+        }
+    
+        pElement.textContent = answer;
+        rElement.textContent = reaction;
+    
+        var cElement = document.getElementById('cprice');
+        cElement.textContent = "Current Exchange Rate (USD): $" + current;
+
+
     }, false);
   }, false);
